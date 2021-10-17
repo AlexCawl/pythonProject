be721e004e18369dd75e5f83ea93fad2023acb09
mick <mdedushkin10@gmail.com>
2021-10-17 21:50:59 +0300

    Сделана 4 лаба

---
+++ Лаба ОП - 4/lab4-5.py
@@ -0,0 +1,23 @@
+#Напишите программу ("lab4-5.py"), которая считывала бы с клавиатуры данные о пяти призывниках
+#(Фамилия, Имя, Отчество, год рождения, заболевание)
+# и выводила бы результат в виде таблицы на экран (считывание и сохранение данных должно производится в словарь).
+
+recruit_table = {}
+print("Начинаем запись: ")
+for i in range(1):
+    print("Призывник №", i + 1, sep='')
+    surname = input("Фамилия:\t")
+    name = input("Имя:\t")
+    second_name = input("Отчество:\t")
+    year = input("Год рождения:\t")
+    disease = input("Заболевание:\t")
+    recruit_table.update({i+1: ' '.join([surname, name, second_name, year, disease])})
+
+for recruits in recruit_table:
+    print(recruits, *recruit_table[recruits])
+
+arr =[]
+tuple = ()
+s = {}
+
+
